af35bfd5392b089bab2bc21ca593650b64a94c66
Bulanov Konstantin <leech001@gmail.com>
2020-11-11 13:24:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# rf-leak-module
-Leak module based on STM32F030F4P6 and NRF24L01
+# Radio module for leak monitoring
+
+The module is a board based on STM32F030F4P6 microcontroller and NRF24L01 radio module.
+The module is designed to detect water leaks and notify the head unit about them.
+This version of the module is powered by the CR2032 battery which should be enough for 6 months of operation.
+
+The default operation logic of the module is as follows:
+1. Deep sleep for one minute;
+2. humidity sensor interrogation;
+3. In case the threshold value is exceeded, send a message via NRF24L01 to the head unit which controls the devices;
+
+## Sensor general circuit
+The radio sensor design itself is on EasyEDA https://easyeda.com/leech001/leakcontrol.
+
+Scheme
+
+![shema](https://raw.githubusercontent.com/leech001/LeakControl/master/RF_module/img/sheme.png)
+
+Board layout
+
+![pcb](https://raw.githubusercontent.com/leech001/LeakControl/master/RF_module/img/pcb.png)
+
+
+Gerber file in gerber directory.
+
+## Software description
+
+A project source code based on the HAL library is available.
+The project itself is generated in STM32CubeMX.
+
+Setting up the pipe identifier
+```
+const uint64_t pipe1 = 0xF0F0F0F0A1LL;
+```
+Sleep Flag Reset
+```
+__HAL_PWR_CLEAR_FLAG(PWR_FLAG_WU);
+```
+Adjusting the sensor threshold
+```
+if (adc > 200) {
+```
+Initialization of the radio module, speed setting, installing the channel and opening the pipe to the transmission.
+```
+NRF_Init();
+setDataRate(RF24_250KBPS);
+setChannel(76);
+openWritingPipe(pipe1);
+```
+
+For those who are not strong, there is a ready-made binary that just needs to be write in microcontroler. Binary directory ``bin``.
+
+Translated with https://www.deepl.com/ru/translator (free version)
---
+++ Src/main.c
@@ -0,0 +1,238 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "adc.h"
+#include "rtc.h"
+#include "spi.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "ee.h"
+#include "nrf24l01.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define VREFINT_CAL_ADDR 0x1FFFF7BA
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+static uint8_t *Unique_ID = (uint8_t *) UID_BASE;
+static uint16_t *vrefint_cal = (uint16_t *) VREFINT_CAL_ADDR;
+
+uint16_t water = 0;
+uint16_t voltage = 0;
+static uint8_t battery[2] = {0,};
+uint8_t nrf_data[32] = {0,};
+
+//        === NRF Setup ===
+//        const uint64_t pipe0 = 0x7878787878LL;
+const uint64_t pipe1 = 0xF0F0F0F0A1LL;
+//        const uint64_t pipe2 = 0xF0F0F0F0B1LL;
+//        const uint64_t pipe3 = 0xF0F0F0F0C1LL;
+//        const uint64_t pipe4 = 0xF0F0F0F0D1LL;
+//        const uint64_t pipe5 = 0xF0F0F0F0E1LL;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_ADC_Init();
+    MX_SPI1_Init();
+    MX_RTC_Init();
+    /* USER CODE BEGIN 2 */
+
+    //Clear standby flag
+    __HAL_PWR_CLEAR_FLAG(PWR_FLAG_WU);
+
+    // Init EEPROM emulation
+    ee_init();
+
+    // Run ADC
+    HAL_ADCEx_Calibration_Start(&hadc);
+    HAL_ADC_Start(&hadc);
+    HAL_ADC_PollForConversion(&hadc, 100);
+    water = HAL_ADC_GetValue(&hadc);
+    HAL_ADC_Start(&hadc);
+    HAL_ADC_PollForConversion(&hadc, 100);
+    voltage = 3300 * (*vrefint_cal) / HAL_ADC_GetValue(&hadc);
+    HAL_ADC_Stop(&hadc);
+
+    ee_read(0, 1, &battery[0]);
+    battery[1] = voltage / 100;
+
+    if (water > 200 || battery[1] < battery[0] || battery[1] > (battery[0] + 5)) {
+        ee_format(false);
+        ee_write(0, 1, &battery[1]);
+        HAL_GPIO_WritePin(NRF_PWR_GPIO_Port, NRF_PWR_Pin, GPIO_PIN_SET);
+        while (!isChipConnected());
+        NRF_Init();
+        setDataRate(RF24_250KBPS);
+        setChannel(76);
+        openWritingPipe(pipe1);
+        maskIRQ(true, true, true);
+
+        // Add message
+        memcpy(nrf_data, Unique_ID, 12);
+        nrf_data[12] = water >> 8;
+        nrf_data[13] = water & 0xff;
+        nrf_data[14] = voltage >> 8;
+        nrf_data[15] = voltage & 0xff;
+
+        for (uint8_t i = 0; i < 5; i++) {
+            write(&nrf_data, 32);
+            HAL_Delay(1000);
+        }
+    }
+//    HAL_PWR_EnterSTANDBYMode();
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        HAL_PWR_EnterSTANDBYMode();
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSI14
+                                       | RCC_OSCILLATORTYPE_LSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSI14State = RCC_HSI14_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.HSI14CalibrationValue = 16;
+    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV8;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
+    PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+/* USER CODE BEGIN 4 */
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
